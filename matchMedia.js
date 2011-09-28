002240aca2e5c30118128793a37e88b24fcfc07c
Chuck Harmston <chuck@chuckharmston.com>
2011-09-28 14:24:57 -0400

    Refactors to use similar interface to browsers

--- matchMedia.js
+++ matchMedia.js
@@ -7,19 +7,28 @@
 
 window.matchMedia = window.matchMedia || (function(doc, undefined){
 
+  var MediaQueryListListener = function(listener){
+    this.handleChange = listener;
+  };
+
+  var MediaQueryList = function(q){
+    this.media = q;
+    this.matches = null;
+    this.addListener = function(listener){
+      //
+    };
+    this.removeListener = function(listener){
+      //
+    };
+  };
+
+
   var bool,
       docElem        = doc.documentElement,
       refNode        = docElem.firstElementChild || docElem.firstChild,
       // fakeBody required for <FF4 when executed in <head>
       fakeBody       = doc.createElement('body'),
-      div            = doc.createElement('div'),
-      _listeners     = [],
-      addListener    = function(){
-
-      },
-      removeListener = function(){
-
-      };
+      div            = doc.createElement('div');
 
   div.id = 'mq-test-1';
   div.style.cssText = "position:absolute;top:-100em";
