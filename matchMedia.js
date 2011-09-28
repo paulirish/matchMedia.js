f926ef3f28015430d72433d245bf73d0c579c270
Chuck Harmston <chuck@chuckharmston.com>
2011-09-28 11:21:37 -0400

    Adds listeners array

--- matchMedia.js
+++ matchMedia.js
@@ -8,11 +8,12 @@
 window.matchMedia = window.matchMedia || (function(doc, undefined){
 
   var bool,
-      docElem  = doc.documentElement,
-      refNode  = docElem.firstElementChild || docElem.firstChild,
+      docElem   = doc.documentElement,
+      refNode   = docElem.firstElementChild || docElem.firstChild,
       // fakeBody required for <FF4 when executed in <head>
-      fakeBody = doc.createElement('body'),
-      div      = doc.createElement('div');
+      fakeBody  = doc.createElement('body'),
+      div       = doc.createElement('div'),
+      _listeners = [];
 
   div.id = 'mq-test-1';
   div.style.cssText = "position:absolute;top:-100em";
