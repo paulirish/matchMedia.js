50fb527bb353e22a56acc54fba083f1afd2fba4a
Scott Jehl <scott@sj.local>
2012-06-07 12:49:06 -0400

    strict compare

--- matchMedia.js
+++ matchMedia.js
@@ -19,7 +19,7 @@ window.matchMedia = window.matchMedia || (function(doc, undefined){
     div.innerHTML = '&shy;<style media="'+q+'"> #mq-test-1 { width: 42px; }</style>';
 
     docElem.insertBefore(fakeBody, refNode);
-    bool = div.offsetWidth == 42;
+    bool = div.offsetWidth === 42;
     docElem.removeChild(fakeBody);
 
     return { matches: bool, media: q };
