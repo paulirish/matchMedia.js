35b041e731ab4a129c354c6042521b90f2fde5a3
Scott Jehl <scott@scottjehl.com>
2012-09-13 11:09:05 -0500

    just some edits to coding style, and added "use strict" for linty freshness.

--- matchMedia.js
+++ matchMedia.js
@@ -1,30 +1,36 @@
 /*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
 
-window.matchMedia = window.matchMedia || (function(doc, undefined){
+window.matchMedia = window.matchMedia || (function( doc, undefined ) {
+
+  "use strict";
 
   var bool,
-      docElem  = doc.documentElement,
-      refNode  = docElem.firstElementChild || docElem.firstChild,
+      docElem = doc.documentElement,
+      refNode = docElem.firstElementChild || docElem.firstChild,
       // fakeBody required for <FF4 when executed in <head>
-      fakeBody = doc.createElement('body'),
-      div      = doc.createElement('div');
+      fakeBody = doc.createElement( "body" ),
+      div = doc.createElement( "div" );
 
-  div.id = 'mq-test-1';
+  div.id = "mq-test-1";
   div.style.cssText = "position:absolute;top:-100em";
   fakeBody.style.background = "none";
   fakeBody.appendChild(div);
 
   return function(q){
 
-    div.innerHTML = '&shy;<style media="'+q+'"> #mq-test-1 { width: 42px; }</style>';
+    div.innerHTML = "&shy;<style media=\"" + q + "\"> #mq-test-1 { width: 42px; }</style>";
 
-    docElem.insertBefore(fakeBody, refNode);
+    docElem.insertBefore( fakeBody, refNode );
     bool = div.offsetWidth === 42;
-    docElem.removeChild(fakeBody);
+    docElem.removeChild( fakeBody );
 
-    return { matches: bool, media: q };
+    return {
+      matches: bool,
+      media: q
+    };
+
   };
 
-}(document));
+}( document ));
 
 
