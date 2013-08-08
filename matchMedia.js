28929cc0260ea14c2de5ec19874c6dbba00f9100
David Knight <dknight@weblinc.com>
2013-08-07 23:35:07 -0400

    Complete rewrite in an effort to gain performance

--- matchMedia.js
+++ matchMedia.js
@@ -1,36 +1,51 @@
 /*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
 
-window.matchMedia = window.matchMedia || (function( doc, undefined ) {
+window.matchMedia || (window.matchMedia = function() {
+    "use strict";
 
-  "use strict";
+    var styleMedia  = (window.styleMedia || window.media);
 
-  var bool,
-      docElem = doc.documentElement,
-      refNode = docElem.firstElementChild || docElem.firstChild,
-      // fakeBody required for <FF4 when executed in <head>
-      fakeBody = doc.createElement( "body" ),
-      div = doc.createElement( "div" );
+    // For those that doen't support matchMedium
+    if (!styleMedia) {
+        var style       = document.createElement('style'),
+            info        = null,
+            setStyle     = function(text) {
+                if (style.styleSheet) {
+                    style.styleSheet.cssText = text;
+                } else {
+                    style.textContent = text;
+                }
+            };
 
-  div.id = "mq-test-1";
-  div.style.cssText = "position:absolute;top:-100em";
-  fakeBody.style.background = "none";
-  fakeBody.appendChild(div);
+        style.type  = 'text/css';
+        style.id    = 'matchmediajs-test';
 
-  return function(q){
+        document.getElementsByTagName('head')[0].appendChild(style);
+        info = ('getComputedStyle' in window) && window.getComputedStyle(style) || style.currentStyle;
 
-    div.innerHTML = "&shy;<style media=\"" + q + "\"> #mq-test-1 { width: 42px; }</style>";
+        styleMedia = {
+            matchMedium: function(media) {
+                var text = '@media ' + media + '{ #matchmediajs-test { width: 1px; } }',
+                    match;
 
-    docElem.insertBefore( fakeBody, refNode );
-    bool = div.offsetWidth === 42;
-    docElem.removeChild( fakeBody );
+                // Add css text
+                setStyle(text);
 
-    return {
-      matches: bool,
-      media: q
+                match = info.width === '1px';
+
+                // remove css text
+                setStyle('');
+
+                return match;
+            }
+        };
+
+    }
+
+    return function(media) {
+        return {
+            matches: styleMedia.matchMedium(media || 'all'),
+            media: media || 'all'
+        };
     };
-
-  };
-
-}( document ));
-
-
+}());
