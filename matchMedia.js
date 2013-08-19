b848cebf97c439b305ebe9cb80853ad02d604113
David Knight <dknight@weblinc.com>
2013-08-18 23:09:19 -0400

    Added 'David Knight' to the authors comment list

--- matchMedia.js
+++ matchMedia.js
@@ -1,4 +1,4 @@
-/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
+/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas, David Knight. Dual MIT/BSD license */
 
 window.matchMedia || (window.matchMedia = function() {
     "use strict";
